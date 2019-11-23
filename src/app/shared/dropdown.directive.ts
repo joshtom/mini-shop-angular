a736888f93c027fe744efefafbbdb891dc4d42ba
joshtom <olajidejoshua4real@gmail.com>
2019-11-23 20:36:17 +0100

    Added appDropdown directive to recipe-detail component

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective {
-
+export class DropdownDirective implements OnInit {
+  // Binding the 'open' with the class object
+  @HostBinding('class.open') isOpen = false;
   constructor() { }
+
+  @HostListener('click') clickEvent(eventData: Event) {
+    this.isOpen = !this.isOpen;
+  }
 
 }
