cb79b0a05904b5a7b68be1a8618761cbe30d2634
joshtom <olajidejoshua4real@gmail.com>
2019-11-23 20:51:53 +0100

    Added elementRef to control the click via document

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, HostListener, HostBinding } from '@angular/core';
+import { Directive, OnInit, HostListener, HostBinding, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
@@ -6,10 +6,17 @@ import { Directive, OnInit, HostListener, HostBinding } from '@angular/core';
 export class DropdownDirective implements OnInit {
   // Binding the 'open' with the class object
   @HostBinding('class.open') isOpen = false;
-  constructor() { }
+  
+  
+  //host listener added to a particular dropdown
+  // @HostListener('click') clickEvent(eventData: Event) {
+  //   this.isOpen = !this.isOpen;
+  // }
 
-  @HostListener('click') clickEvent(eventData: Event) {
-    this.isOpen = !this.isOpen;
+  //HostListener added to the document that closes every other dropdown upon click on the document
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+  constructor(private elRef: ElementRef) { }
 
 }
