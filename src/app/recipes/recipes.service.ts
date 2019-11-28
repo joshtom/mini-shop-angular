9a6853810e152ce026bb18e8c2430e9d8f6515ac
joshtom <olajidejoshua4real@gmail.com>
2019-11-28 01:40:02 +0100

    Updated getRecipe function

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -7,12 +7,12 @@ import { Recipe } from './recipe.model';
 export class RecipesService {
 
   constructor() { }
-  recipes: Recipe[] = [
+  private recipes: Recipe[] = [
     new Recipe('Sausage', 'Sausage Shrimp & Chicken', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg'),
     new Recipe('Eggplant Parmesan', 'This is my favorite way to make eggplant parmesan', 'https://www.tablefortwoblog.com/wp-content/uploads/2018/05/eggplant-parmesan-recipe-photos-tablefortwoblog-1.jpg')
   ];
 
   getRecipe() {
-    this.recipes.slice();
+    return this.recipes.slice();
   }
 }
