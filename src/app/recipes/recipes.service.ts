578df78224a403de85a2afa9b9185d36a76bdb9b
joshtom <olajidejoshua4real@gmail.com>
2019-11-28 01:39:29 +0100

    Added getRecipe function in order not to access the array directly

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -11,4 +11,8 @@ export class RecipesService {
     new Recipe('Sausage', 'Sausage Shrimp & Chicken', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg'),
     new Recipe('Eggplant Parmesan', 'This is my favorite way to make eggplant parmesan', 'https://www.tablefortwoblog.com/wp-content/uploads/2018/05/eggplant-parmesan-recipe-photos-tablefortwoblog-1.jpg')
   ];
+
+  getRecipe() {
+    this.recipes.slice();
+  }
 }
