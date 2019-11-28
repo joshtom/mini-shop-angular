ba91b66e87a6fa486ce6cdc76ea946cc3d894698
joshtom <olajidejoshua4real@gmail.com>
2019-11-28 21:17:27 +0100

    Added eventemmiter to the shopping-list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter, Output } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  @Output() IngredientEnter = new EventEmitter<Ingredient>()
   private ingredients: Ingredient[] = [
     new Ingredient('Maggi', 300),
     new Ingredient('Gino', 70),
