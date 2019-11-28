d74744cf825ab7edb8640fc8c253d720a86f7f3a
joshtom <olajidejoshua4real@gmail.com>
2019-11-28 22:15:08 +0100

    Added array of ingredients for the first Recipe collection

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,19 @@ export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>();
   constructor() { }
   private recipes: Recipe[] = [
-    new Recipe('Sausage', 'Sausage Shrimp & Chicken', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg'),
-    new Recipe('Eggplant Parmesan', 'This is my favorite way to make eggplant parmesan', 'https://www.tablefortwoblog.com/wp-content/uploads/2018/05/eggplant-parmesan-recipe-photos-tablefortwoblog-1.jpg')
+    new Recipe('Sausage', 
+    'Sausage Shrimp & Chicken', 
+    'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg',
+    [
+      new Ingredient('Maggi', 10),
+      new Ingredient('Chicken', 1)
+    ]
+    ),
+    new Recipe('Eggplant Parmesan',
+     'This is my favorite way to make eggplant parmesan', 
+     'https://www.tablefortwoblog.com/wp-content/uploads/2018/05/eggplant-parmesan-recipe-photos-tablefortwoblog-1.jpg',
+     []
+     )
   ];
 
   getRecipes() {
