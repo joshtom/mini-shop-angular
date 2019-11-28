9d85f8d216331a696b6a5f80c189de0410ea7cac
joshtom <olajidejoshua4real@gmail.com>
2019-11-28 17:29:39 +0100

    Added ingredient model array into shopping-list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Maggi', 300),
+    new Ingredient('Gino', 70),
+  ];
   constructor() { }
 }
