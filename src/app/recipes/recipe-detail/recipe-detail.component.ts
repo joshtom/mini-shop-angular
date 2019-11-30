0f9a6db00350a4451c43502e20f299d8511d0ab8
joshtom <olajidejoshua4real@gmail.com>
2019-11-30 03:34:58 +0100

    changed the click method to onAddToShoppingList

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,9 +9,14 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() getShoppingList = new EventEmitter<Ingredient[]>();
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  addToShoppingList(){
+    this.getShoppingList.emit(this.recipe.ingredient);
     
   }
 
