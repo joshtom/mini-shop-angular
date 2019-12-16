2b9c96b606b5ff7675090fd586848bd32f6ec838
joshtom <olajidejoshua4real@gmail.com>
2019-12-16 23:21:08 +0100

    Added children in RecipesComponent app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipe', component: RecipesComponent },
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },  
+    { path: 'recipe', component: RecipesComponent, children: [
+        { path: '', component: RecipeStartComponent }
+    ] },
     { path: 'shopping-list', component: ShoppingListComponent },
     // { path: '**', component: Error404Component }
   ];
