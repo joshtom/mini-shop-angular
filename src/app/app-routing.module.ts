fefcc91806ee80eaf45887ea000e9ff72d61bd86
joshtom <olajidejoshua4real@gmail.com>
2019-12-16 23:25:20 +0100

    Added RecipDetailCompnent in the child route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },  
     { path: 'recipe', component: RecipesComponent, children: [
-        { path: '', component: RecipeStartComponent }
+        { path: '', component: RecipeStartComponent },
+        { path: ':id', component: RecipeDetailComponent }
     ] },
     { path: 'shopping-list', component: ShoppingListComponent },
     // { path: '**', component: Error404Component }
