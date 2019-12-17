652bc2222f37bd1c6aecaa6b526b0da7945703ca
joshtom <olajidejoshua4real@gmail.com>
2019-12-17 19:59:29 +0100

    Added recipe-edit component to the recipe children route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },  
     { path: 'recipe', component: RecipesComponent, children: [
         { path: '', component: RecipeStartComponent },
+        { path: '', component: RecipeEditComponent },
         { path: ':id', component: RecipeDetailComponent }
     ] },
     { path: 'shopping-list', component: ShoppingListComponent },
