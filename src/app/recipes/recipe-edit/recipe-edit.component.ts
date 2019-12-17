8da523aad74b861abcaabdc1b52132fda7f894f9
joshtom <olajidejoshua4real@gmail.com>
2019-12-17 20:21:14 +0100

    Retrieved route parameters

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,11 +10,22 @@ export class RecipeEditComponent implements OnInit {
   /**
    * Listed Below is the process for retrieving routes dynamically
    * 1. Activated Route must be injected into the constructor method
-   * 
+   * 2. The ID can be retrieved in the ngOnInit method
    */
+  id: number;
+  editMode = false; // To confirm which state the edit route is;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.id = +params['id']; // Converting the ID to number
+        this.editMode = params['id'] != null; // Check if the id is not empty then update the state of the editMode;
+        // console.log(this.editMode);
+
+      }
+    )
   }
 
 }
